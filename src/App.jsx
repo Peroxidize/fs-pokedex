f87db601d078cc80a3aa0652f05ad155ce7233db
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-03-31 22:52:02 +0800

    bump for tag versioning

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ const App = () => {
     const pokemonId = pokemonList.find(
       ({ name }) => name === match.params.name
     ).id;
+
     previous = pokemonList.find(({ id }) => id === pokemonId - 1);
     next = pokemonList.find(({ id }) => id === pokemonId + 1);
   }
